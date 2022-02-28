6e596d50b08471d219d4170ad045543086f9984c
bitri12 <60946412+bitri12@users.noreply.github.com>
2022-02-28 11:31:18 +0100

    debut menu

--- includes/menu.sh
+++ includes/menu.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 menu() {
-    echo "menu"
+    while [ $choix -ne 0]
+    do
+       
+
+       case $choix in
+        0) echo "Aurevoir !" ;;
+        *) echo "Erreur dans votre selection !" ;;
+        esac
+    done
 }
