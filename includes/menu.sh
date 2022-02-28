c06c9a4ce10f30eb46d985f6e3c5ce27714a1909
bitri12 <60946412+bitri12@users.noreply.github.com>
2022-02-28 12:16:29 +0100

    debut menu et reparation

--- includes/menu.sh
+++ includes/menu.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 menu() {
-    while [ $choix -ne 0]
+    while [[ $choix -ne 0 ]]
     do
-       
-
+        echo "--------- Menu --------"
+        echo "0) Quitter le Programme"
+        echo "1)"
+        echo "2)"
+        echo "3)"
+        echo "4)"
+        echo "5)"
+        echo "6)"
+        read -p "Option" choix
        case $choix in
-        0) echo "Aurevoir !" ;;
-        *) echo "Erreur dans votre selection !" ;;
+            0) echo "Aurevoir !" ;;
+            *) echo "L'option que vous avez selectionner n'existe pas !" ;;
         esac
     done
+    return 0
 }
--- trace.sh
+++ trace.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-source ./includes/fonctionalite.sh
-source ./includes/menu.sh
-source ./includes/menu_graphique.sh
+source includes/fonctionalite.sh
+source includes/menu.sh
+source includes/menu_graphique.sh
 
 if [ $# -eq 1 ]; then
    while getopts "hvmg" var
